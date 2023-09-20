01eba7f294cfae24c0ab3fe81b5fac4ded47255b
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-09-20 16:23:00 +0200

    all for today

--- index.js
+++ index.js
@@ -3,19 +3,96 @@ const modal = document.getElementById('modal')
 const enterNewBookBtn = document.getElementById('enterBtn')
 const addNewBookBtn = document.getElementById('addNewBookBtn')
 const overlay = document.querySelector('.overlay')
+const isReadBtn = document.querySelectorAll('.book[class="isRead"]')
+const library = JSON.parse(localStorage.getItem('library')) || []
 
 function openModal() {
+    addBookForm.reset()
     modal.classList.add('active')
     overlay.classList.add('active')
 }
 function closeModal() {
-    addBookForm.reset()
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+function displayLibrary() {
+    const grid = document.querySelector('#grid')
+    const resetBookGrid = () => {
+        grid.innerHTML = ''
+    }
+    const gridCreation = (item) => {
+        const card = document.createElement('div')
+        const title = document.createElement('p')
+        const author = document.createElement('p')
+        const readBtn = document.createElement('button')
+        const isRead = item.isRead ? 'Read' : 'Not Read'
 
+        grid.appendChild(card)
+        card.appendChild(title)
+        card.appendChild(author)
+        card.appendChild(readBtn)
+
+        title.textContent = item.title
+        author.textContent = item.author
+        readBtn.textContent = isRead
+        readBtn.onclick = changeRead
+
+        card.classList.add('book')
+        readBtn.classList.add('isRead')
+        item.isRead ? readBtn.classList.add('active') : ''
+
+    }
+    resetBookGrid()
+    for (const book of library) {
+        gridCreation(book)
+    }
+}
+
+function Book(
+    title = 'No Title',
+    author = 'No Author',
+    isRead = false
+) {
+    this.title = title
+    this.author = author
+    this.isRead = isRead
+}
+
+function enterNewBook(e) {
+    e.preventDefault()
+    const title = document.querySelector('#title').value
+    const author = document.querySelector('#author').value
+    const isRead = document.querySelector('#isRead').checked
+    const newBook = new Book(title, author, isRead)
+    library.push(newBook)
+    closeModal()
+    displayLibrary()
+    saveLocal()
+}
+function changeRead() {
+    const title = this.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        ''
+    )
+    const findBook = () => {
+        return library.find(book => book.title === title)
+    }
+    const book = findBook()
+    book.isRead = !book.isRead
+    displayLibrary()
+    saveLocal()
+}
+
+enterNewBookBtn.addEventListener('click', enterNewBook)
+isReadBtn.forEach(btn => btn.addEventListener('click', changeRead))
 addNewBookBtn.addEventListener('click', openModal)
 overlay.addEventListener('click', closeModal)
+
+//local Storage
+displayLibrary()
+const saveLocal = () => {
+    localStorage.setItem('library', JSON.stringify(library))
+}
 //Book constructor
 /* class Book {
     constructor(title, author) {
